59586d699a5e4bc21aebc077fdd7dfd39effff76
LucasFerreiraPrazeres <luucasfp0@gmail.com>
2021-03-03 09:23:58 -0300

    exercicio_02 Listar Diretorios

---
+++ listar_diretorios.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+read -p "digite um diretorio:" diretorio01
+ls ${diretorio01}
+
+read -p "digite um diretorio:" diretorio02
+ls ${diretorio02}
+
+read -p "digite um diretorio:" diretorio03
+ls ${diretorio03}
+
+
